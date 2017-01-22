c1582e202c0af522fbec09b51c6ba20f4fdc5bf4
Saba <nigistsaba@hotmail.com>
2017-01-22 21:47:07 +0100

    hard coding of speech rating for testing

--- src/main/java/com/senacor/DevConRatingServiceApplication.java
+++ src/main/java/com/senacor/DevConRatingServiceApplication.java
@@ -1,6 +1,7 @@
 package com.senacor;
 
 import com.senacor.model.NaturalPerson;
+import com.senacor.model.SpeechRating;
 import com.senacor.repository.NaturalPersonRepository;
 import com.senacor.repository.SpeechRatingRepository;
 import com.senacor.repository.SpeechRepository;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootApplication
 public class DevConRatingServiceApplication implements CommandLineRunner {
@@ -30,5 +34,15 @@ public class DevConRatingServiceApplication implements CommandLineRunner {
 	public void run(String... strings) throws Exception {
 		/*speechRatingRepository.deleteAll();
 		speechRepository.deleteAll();*/
+
+		List<SpeechRating>speechRatings = speechRatingRepository.findBySpeech(speechRepository.findBySpeechId("588511dc06d4cd204870e090"));
+		
+		for (SpeechRating speechRating: speechRatings) {
+			speechRating.setRating(3);
+			speechRatingRepository.save(speechRating);
+		}
+
+
+
 	}
 }
